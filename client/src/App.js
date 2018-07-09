89e589fd5ff070b431d31e3efb82b88c53871f07
CuHTeTuk <cuhtetuk@CuHTeTuks-MBP.attlocal.net>
2018-07-08 19:43:32 -0500

    moved charts to dashboard removed the old stuff

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 import Walkthrough from "./components/Walkthrough";
 import AppNavbar from "./components/AppNavbar";
-import PieGraph from "./components/PieGraph";
-import LineGraph from "./components/LineGraph";
+// import PieGraph from "./components/PieGraph" moved to totalspending component;
+// import LineGraph from "./components/LineGraph"; moved to CashFlow component
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import dashboard from './Pages/dashboard';
 import NavTabs from './components/NavTabs';
@@ -22,8 +22,8 @@ return (
 <Router>
 <div>
 <NavTabs/>
-<Route path="/" component={PieGraph} />
-<Route path="/" component={LineGraph} />
+{/* <Route path="/" component={PieGraph} />  moved to totalspending component*/}
+{/* <Route path="/" component={LineGraph} />   CashFlow component*/} 
 <Route exact path="/" component={AppNavbar} />
 <Route exact path="/" component={Walkthrough} />
 <Route exact path="/dashboard" component={dashboard} />
