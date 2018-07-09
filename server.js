b6b8d1a3165bc0a870f9aa23359447abd90e8bcf
Thais Cailet <thais.cailet@gmail.com>
2018-07-08 20:08:04 -0500

    #46 Abstracts routes from PlaidController

--- server.js
+++ server.js
@@ -1,80 +1,69 @@
-const express = require('express');
+require('dotenv').config();
 const https = require('https');
+const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const mongoose = require('mongoose');
 const app = express();
+const routes_walker = require('node-walker');
 
-const PlaidController = require('./api/controllers/plaidController');
+//const PlaidController = require('./api/controllers/plaidController');
 //const SwaggerExpress = require('swagger-express-mw');
+global.__basedir = __dirname;
 global.__plaidClient = false;
-require('dotenv').config();
 
 //------------ MIDDLEWARE -------------
-app.use(
-  express.urlencoded({extended: true})
-);
+app.use(express.urlencoded({extended: true}));
 
 app.use(express.json());
 app.use(express.static("client/build"));
 
+app.get("/", (req, res) => res.send("THE APP IS ONLINE"));
+
 const httpsOptions = {
-     key: fs.readFileSync('./security/cert.key'),
-     cert: fs.readFileSync('./security/cert.pem')
+    key: fs.readFileSync('./security/cert.key'),
+    cert: fs.readFileSync('./security/cert.pem')
 };
 
 //----------- ROUTING ---------------------
-const users = require("./routes/api/users");
 
-app.get("/", (req, res) => res.send("THE APP IS ONLINE MUTHUFUCKAAAA"));
-app.use("/api/users", users);
+routes_walker( './api/routes',
+    function (errorObject, fileName, fnNext) {
+        if (errorObject) throw errorObject;
+        if (fileName !== null) {
+            const route = require(fileName);
+            route.register(express);
+            console.log(fileName);
+            console.log(__basedir.length);
+            //console.log(`Route ${fileName.substr(__basedir.length +1)} included.`);
+        }
+        if (fileName === null) {
+
+            return;
+        }
+        if (fnNext) fnNext();
+        else {
+            console.log('All routes included');
+            const BPORT = process.env.BPORT || 3001;
+            const server = https.createServer(httpsOptions, app).listen(BPORT, () => {
+
+                console.log(`Secure server listening on port ${BPORT}`);
+            });
+
+        }
+    }
+);
 
 //------------ DATABASE -----------------------
 const db = require("./config/keys").mongoURI;
 
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
-
-app.post("/api/get_access_token", function(request, response, next) {
-  global.__plaidClient = new PlaidController();
-  __plaidClient.publicToken = request.body.public_token;
-  __plaidClient.getAccessToken().then(link => response.json(link));
-});
-
-app.get("/api/accounts", function(request, response, next) {
-  // Retrieve high-level account information and account and routing numbers
-  // for each account associated with the Item.
-  __plaidClient
-    .getAccountInfo(__plaidClient.accessToken)
-    .then(accountinfo => response.json(accountinfo));
-});
-
-app.post("/api/item", function(request, response, next) {
-  __plaidClient
-    .getItem(__plaidClient.accessToken)
-    .then(itemInfo => response.json(itemInfo));
-});
-
-app.post("/api/transactions", function(request, response, next) {
-  __plaidClient.transactionDaysAgo = 30;
-  __plaidClient
-    .getTransactions()
-    .then(transactions => response.json(transactions));
-});
-
-
-const PORT = process.env.PORT || 3001;
+    .connect(
+        db,
+        { useNewUrlParser: true }
+    )
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
 
 //app.listen(PORT, () => {console.log(`SERVER LISTENING ON PORT: ${PORT}`)});
-
-const server = https.createServer(httpsOptions, app).listen(PORT, () => {
-    console.log(`Secure server listening on port ${PORT}`);
-});
-
 module.exports = app;
-
