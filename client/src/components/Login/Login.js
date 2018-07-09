c14864c5252666eab4ad9ebe26024e2225914a22
wabernathy96 <wabernathy96@gmail.com>
2018-07-08 20:08:09 -0500

    Login form complete // Needs to be added to login page

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,130 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(this.state.email);
+    event.preventDefault();
+
+    // Post to login api route in order to authenticate user
+    axios
+      .post("/api/users/login", {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(response => {
+        if (!response.data.errmsg) {
+          console.log("successful login");
+          this.setState({
+            //redirect to dashboard page
+            redirectTo: "/dashboard"
+          });
+        } else {
+          console.log("Email or Password incorrect, please try again.");
+          this.setState({
+            //redirect to login page
+            redirectTo: "/login"
+          });
+        }
+      })
+      .catch(errors => {
+        console.log(`Login error: ${errors}`);
+      });
+  }
+
+  render() {
+    return (
+      <div className="LoginForm">
+        <form className="form-horizontal">
+          <div className="form-group">
+            <div className="col-1 col-ml-auto">
+              <label className="form-label" htmlFor="name">
+                Name:
+              </label>
+            </div>
+            <div className="col-3 col-mr-auto">
+              <input
+                className="form-input"
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Name"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <div className="col-1 col-ml-auto">
+              <label className="form-label" htmlFor="email">
+                Email:
+              </label>
+            </div>
+            <div className="col-3 col-mr-auto">
+              <input
+                className="form-input"
+                type="text"
+                id="email"
+                name="email"
+                placeholder="Email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <div className="col-1 col-ml-auto">
+              <label className="form-label" htmlFor="password">
+                Password:{" "}
+              </label>
+            </div>
+            <div className="col-3 col-mr-auto">
+              <input
+                className="form-input"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <div className="col-7" />
+            <button
+              className="btn btn-primary col-1 col-mr-auto"
+              onClick={this.handleSubmit}
+              type="submit"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
--- server.js
+++ server.js
@@ -1,32 +1,28 @@
-const express = require('express');
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
-const mongoose = require('mongoose');
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+const mongoose = require("mongoose");
 const app = express();
-
-const PlaidController = require('./api/controllers/plaidController');
 //const SwaggerExpress = require('swagger-express-mw');
 global.__plaidClient = false;
-require('dotenv').config();
+require("dotenv").config();
 
 //------------ MIDDLEWARE -------------
-app.use(
-  express.urlencoded({extended: true})
-);
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(express.static("client/build"));
 
 const httpsOptions = {
-     key: fs.readFileSync('./security/cert.key'),
-     cert: fs.readFileSync('./security/cert.pem')
+  key: fs.readFileSync("./security/cert.key"),
+  cert: fs.readFileSync("./security/cert.pem")
 };
 
 //----------- ROUTING ---------------------
 const users = require("./routes/api/users");
 
-app.get("/", (req, res) => res.send("THE APP IS ONLINE MUTHUFUCKAAAA"));
+app.get("/", (req, res) => res.send("THE APP IS ONLINE"));
 app.use("/api/users", users);
 
 //------------ DATABASE -----------------------
@@ -39,6 +35,9 @@ mongoose
   )
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+
+//----------- PLAID ROUTES ------------------------
+const PlaidController = require("./api/controllers/plaidController");
 
 app.post("/api/get_access_token", function(request, response, next) {
   global.__plaidClient = new PlaidController();
@@ -67,14 +66,13 @@ app.post("/api/transactions", function(request, response, next) {
     .then(transactions => response.json(transactions));
 });
 
-
+//--------- START SERVER -----------------------
 const PORT = process.env.PORT || 3001;
 
 //app.listen(PORT, () => {console.log(`SERVER LISTENING ON PORT: ${PORT}`)});
 
 const server = https.createServer(httpsOptions, app).listen(PORT, () => {
-    console.log(`Secure server listening on port ${PORT}`);
+  console.log(`Secure server listening on port ${PORT}`);
 });
 
 module.exports = app;
-
