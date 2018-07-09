02d833c2e495ac27c0683ea9db0633be4aa0736b
CuHTeTuk <cuhtetuk@CuHTeTuks-MBP.attlocal.net>
2018-07-08 21:19:51 -0500

    removed old nvigation bar added btn and currently working on fixing back btn

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,41 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Container
-} from 'reactstrap';
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from "reactstrap";
 
 export default class AppNavbar extends Component {
-    state = {
-        isOpen: false
-    };
+  state = {
+    isOpen: false
+  };
 
-    render() {
-        return (
-            <div>
-                <Navbar color="dark" dark expand="sm" className="mb-5">
-                    <Container>
-                        <NavbarBrand href="/">MoneyTree
-                        </NavbarBrand>
-                        <NavbarToggler onClick={this.toggle} />
-                        <Collapse isOpen={this.state.isOpen} navbar>
-                            <Nav className="ml-auto" navbar>
-                                <NavItem>
-                                    <NavLink id="link-btn" href="#">Link Account</NavLink>
-                                    <NavLink id="get-accounts-btn" href="#">Get Accounts</NavLink>
-                                    <NavLink id="get-item-btn" href="#">Get Item</NavLink>
-                                    <NavLink id="get-transactions-btn" href="#">Get Transactions</NavLink>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
-                    </Container>
-                </Navbar>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <p className="text-center">
+         
+          after switching from home to dashboard and back to home page please
+          refresh the page, add account or itmes wouldnt work: current issue.
+          THANKS
+        </p>
+        <div className="container">
+          <div>
+            <button
+              type="button"
+              className="btn btn-outline-primary"
+              id="link-btn"
+            >
+              Link Account
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-secondary"
+              id="get-accounts-btn"
+            >
+              Get Accounts
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-success"
+              id="get-item-btn"
+            >
+              Get Item
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-danger"
+              id="get-transactions-btn"
+            >
+              Get Transactions
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
