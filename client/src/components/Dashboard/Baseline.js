f081ecb17b252ad66084650e646dbeaa38035b7c
Sneha Dama <snehadama@Snehas-Air.lan>
2018-07-09 10:00:19 -0500

    Updated tamagotchi with new avatar

---
+++ client/src/components/Dashboard/Baseline.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+//import './App.css';
+// import Walkthrough from './components/Walkthrough';
+//import AppNavbar from './components/AppNavbar';
+//import DataBody from './components/DataBody';
+
+
+
+class Userdash extends Component {
+
+
+    render() {
+        return (
+            <div>
+                testing
+            </div>
+        )
+    }
+
+}
+export default Userdash;
