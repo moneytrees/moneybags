a9be044901f2b3b18f0c61076449b6b7d005da7c
Thais Cailet <thais.cailet@gmail.com>
2018-07-11 15:03:39 -0500

    Adds missing Login form

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -28,9 +28,53 @@ class Login extends Component {
             return <Redirect to={from}/>;
 
         return (
-            <div>
-                <p>You must log in to view the page</p>
-                <button onClick={this.login}>Log in</button>
+            <div className="LoginForm">
+                <form className="form-horizontal" onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <div className="col-1 col-ml-auto">
+                            <label className="form-label" htmlFor="email">
+                                Email:
+                            </label>
+                        </div>
+                        <div className="col-3 col-mr-auto">
+                            <input
+                                className="form-input"
+                                type="text"
+                                id="email"
+                                name="email"
+                                placeholder="Email"
+                                value={this.state.email}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <div className="col-1 col-ml-auto">
+                            <label className="form-label" htmlFor="password">
+                                Password:{" "}
+                            </label>
+                        </div>
+                        <div className="col-3 col-mr-auto">
+                            <input
+                                className="form-input"
+                                type="password"
+                                id="password"
+                                name="password"
+                                placeholder="Password"
+                                value={this.state.password}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <div className="col-7" />
+                        <button className="btn btn-primary col-1 col-mr-auto" type="submit">
+                            Login
+                        </button>
+                    </div>
+                </form>
             </div>
         );
     }
@@ -120,53 +164,53 @@ export default Login;
   render() {
     return (
       <div className="LoginForm">
-        <form className="form-horizontal" onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <div className="col-1 col-ml-auto">
-              <label className="form-label" htmlFor="email">
-                Email:
-              </label>
-            </div>
-            <div className="col-3 col-mr-auto">
-              <input
-                className="form-input"
-                type="text"
-                id="email"
-                name="email"
-                placeholder="Email"
-                value={this.state.email}
-                onChange={this.handleChange}
-              />
-            </div>
-          </div>
+   <form className="form-horizontal" onSubmit={this.handleSubmit}>
+   <div className="form-group">
+   <div className="col-1 col-ml-auto">
+   <label className="form-label" htmlFor="email">
+   Email:
+   </label>
+   </div>
+   <div className="col-3 col-mr-auto">
+   <input
+   className="form-input"
+   type="text"
+   id="email"
+   name="email"
+   placeholder="Email"
+   value={this.state.email}
+   onChange={this.handleChange}
+   />
+   </div>
+   </div>
 
-          <div className="form-group">
-            <div className="col-1 col-ml-auto">
-              <label className="form-label" htmlFor="password">
-                Password:{" "}
-              </label>
-            </div>
-            <div className="col-3 col-mr-auto">
-              <input
-                className="form-input"
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Password"
-                value={this.state.password}
-                onChange={this.handleChange}
-              />
-            </div>
-          </div>
+   <div className="form-group">
+   <div className="col-1 col-ml-auto">
+   <label className="form-label" htmlFor="password">
+   Password:{" "}
+   </label>
+   </div>
+   <div className="col-3 col-mr-auto">
+   <input
+   className="form-input"
+   type="password"
+   id="password"
+   name="password"
+   placeholder="Password"
+   value={this.state.password}
+   onChange={this.handleChange}
+   />
+   </div>
+   </div>
 
-          <div className="form-group">
-            <div className="col-7" />
-            <button className="btn btn-primary col-1 col-mr-auto" type="submit">
-              Login
-            </button>
-          </div>
-        </form>
-      </div>
+   <div className="form-group">
+   <div className="col-7" />
+   <button className="btn btn-primary col-1 col-mr-auto" type="submit">
+   Login
+   </button>
+   </div>
+   </form>
+   </div>
     );
   }*/
 
