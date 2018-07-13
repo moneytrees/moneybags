7e8639644d13e8632d2eaab8c8912b18f3a5eec2
Vizena2005 <vizena2005@gmail.com>
2018-07-12 20:51:07 -0500

    adding comments with all stats

--- client/src/Pages/Unprotected/Team/Team.js
+++ client/src/Pages/Unprotected/Team/Team.js
@@ -1,97 +1,34 @@
 import React, { Component } from "react";
-import { Button } from "reactstrap";
 
 class Team extends Component {
   render() {
+    // Names and positions:
+
+    // Addis Casco: Brand Manager/Front-End
+    // Sneha Dama: Brand Manager/Front-End
+    // Molly Cougill: Flex-PM/Full-Stack
+    // Caro Ramirez: Brand Manager/QA/QC
+    // Natalie Hoben: PM/Front-End
+    // Thais Cailet: Flex-Deployment Manager/Back-End
+    // Chad Alessi: SCRUM Master/Full-Stack
+    // Yuta Tamura: DBA/Back-End
+    // Sergey Nikitin: Deployment Manager/Full-Stack
+    // Wesley Jackson: DBA/Full-Stack
+    // Brant Campodonico: DBA/Full-Stack
+    // Ejeme Ogedengbe: SCRUM Master/QA/QC
+    // Juhi Patel: SCRUM Master/QA/QC
+    // Marilyn Blythe: DBA/Full-Stack
+    // Vinanti Naik: Deployment Manager/Full-Stack
+    // Rohan Menon: Feature Owner/Back-End
+    // Will Abernathy: Flex-PM/Full-Stack
+    // Chris Jones: Feature Owner/Back-End
+    // David Vizena: Brand Manager/Front-End
+    // Connor Thomas: Feature Owner/Full-Stack
+    // Christian Ngo: QA/QC/Front-End
+
     return (
       <div>
-        <div className="row">
-          <div className="col-4">
-            <h1>Front-End Developers</h1>
-            <p className="text-center">David Vizena</p>
-            <p className="text-center">Molly Cougil</p>
-            <p className="text-center">Sneha Dama</p>
-            <p className="text-center">Addis Casco</p>
-            <p className="text-center">Natalie Hoben</p>
-            <p className="text-center">Christian Ngo</p>
-          </div>
-          <div className="col-4">
-            <h1>Back-End Developers</h1>
-            <p className="text-center">Thais Cailet</p>
-            <p className="text-center">Yuta Tamura</p>
-            <p className="text-center">Rohan Menon</p>
-            <p className="text-center">Chris Jones</p>
-          </div>
-          <div className="col-4">
-            <h1>Brand Manager</h1>
-            <p className="text-center">Addis Casco</p>
-            <p className="text-center">Sneha Dama</p>
-            <p className="text-center">Caro Ramirez</p>
-            <p className="text-center">David Vizena</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-4">
-            <h1>Full-Stack</h1>
-            <p className="text-center">Molly Cougil</p>
-            <p className="text-center">Chad Alessi</p>
-            <p className="text-center">Sergey Nikitin</p>
-            <p className="text-center">Wesley Jackson</p>
-            <p className="text-center">Brant Campodonico</p>
-            <p className="text-center">Marilyn Blythe</p>
-            <p className="text-center">Vinanti Naik</p>
-            <p className="text-center">Will Abernathy</p>
-            <p className="text-center">Connor Thomas</p>
-          </div>
-          <div className="col-4">
-            <h1>QA/QC</h1>
-            <p className="text-center">Caro Ramirez</p>
-            <p className="text-center">Ejeme Ogedengbe</p>
-            <p className="text-center">Juhi Patel</p>
-          </div>
-          <div className="col-4">
-            <h1>Flex-PM</h1>
-            <p className="text-center">Molly Cougil</p>
-            <p className="text-center">Will Abernathy</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-4">
-            <h1>PM</h1>
-            <p className="text-center">Natalie Hoben</p>
-          </div>
-          <div className="col-4">
-            <h1>SCRUM Master</h1>
-            <p className="text-center">Chad Alessi</p>
-            <p className="text-center">Ejeme Ogedengbe</p>
-            <p className="text-center">Juhi Patel</p>
-          </div>
-          <div className="col-4">
-            <h1>Deployment Manager</h1>
-            <p className="text-center">Molly Cougil</p>
-            <p className="text-center">Will Abernathy</p>
-          </div>
-        </div>
-        {/* ALKSJDLKJ */}
-        <div className="row">
-          <div className="col-4">
-            <h1>Flex - Deployment Manager</h1>
-            <p className="text-center">Thais Cailet</p>
-          </div>
-          <div className="col-4">
-            <h1>DBA</h1>
-            <p className="text-center">Yuta Tamura</p>
-            <p className="text-center">Wesley Jackson</p>
-            <p className="text-center">Brant Campodonico</p>
-            <p className="text-center">Marilyn Blythe</p>
-          </div>
-          <div className="col-4">
-            <h1>Feature Owner</h1>
-            <p className="text-center">Rohan Menon</p>
-            <p className="text-center">Chris Alessi</p>
-            <p className="text-center">Connor Thomas</p>
-          </div>
-        </div>
+        <p>Test</p>
       </div>
     );
   }
