b04d749c76a467a08b671d2d36810930cc39491c
Thais Cailet <thais.cailet@gmail.com>
2018-07-13 17:47:00 -0500

    tries to repair commits

--- client/src/Pages/Unprotected/Access/Userlogin.js
+++ client/src/Pages/Unprotected/Access/Userlogin.js
@@ -2,15 +2,42 @@ import React, { Component } from "react";
 import Login from "../../../components/Login";
 
 class UserLogin extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Money Tree: User Login</h1>
 
-        <Login />
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.getFeedback = this.getFeedback.bind(this);
+    }
+
+    getFeedback() {
+        if(this.props.location.state){
+            const { from } = this.props.location.state;
+            let userfeedback  = '';
+            if(from) {
+                switch(from.pathname){
+                    case '/':
+                    case '/login':
+                        userfeedback = '';
+                        break;
+                    case '/register':
+                        userfeedback = from.pathname.feedback;
+                        break;
+                    default:
+                        userfeedback = 'You must be logged in to access this page';
+                }
+            }
+
+            return  <h2>{userfeedback}</h2>;
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>Money Tree: User Login</h1>
+                {this.getFeedback()}
+                <Login location={this.props.location || null}/>
+            </div>
+        );
+    }
 }
 
 export default UserLogin;
