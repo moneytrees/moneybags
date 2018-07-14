848d12923a6f543acd850a0d0e017ad9bd817f04
Vizena2005 <vizena2005@gmail.com>
2018-07-14 09:20:46 -0500

    fixing links for students

--- client/src/Pages/Protected/Dashboard/Dashboard.js
+++ client/src/Pages/Protected/Dashboard/Dashboard.js
@@ -5,22 +5,17 @@ import CashFlow from "../../../components/UserDashboard/CashFlow";
 import TotalSpending from "../../../components/UserDashboard/TotalSpending";
 import TransactionDetail from "../../../components/UserDashboard/TransactionDetail";
 import Achievement from "../../../components/Achievements";
-import ProgressBar from "../../../components/ProgressBar"
-import { Card, CardImg } from 'reactstrap';
+import ProgressBar from "../../../components/ProgressBar";
+import { Card, CardImg } from "reactstrap";
 import { Animated } from "react-animated-css";
-import Zoom from 'react-reveal/Zoom';
-import Fade from 'react-reveal/Fade';
+import Zoom from "react-reveal/Zoom";
+import Fade from "react-reveal/Fade";
 import "./Dashboard.css";
 
-
 class Dashboard extends Component {
-
   render() {
     return (
-
-
       <div className="background">
-
         {/* <h1>
 
           Money Tree: User Dashboard
@@ -30,95 +25,84 @@ class Dashboard extends Component {
         {/* <Button color="danger">Danger!</Button> */}
         <div className="col-12">
           <div className="container">
-
             <div className="Acheivement">
-
-
-
               <div className="row">
-
-               
-                  <div className="col-md-4">
-                  <Animated animationIn="slideInDown" animationOut="zoomOutDown" isVisible={true}>
-                    
-                      {/* <h1> BigFoot Avatar </h1> */}
-                      <img className="neutralBigFoot" src={neutralBigFoot} alt="logo" />
-                    
-                    </Animated>
-                  </div>
-                
-
+                <div className="col-md-4">
+                  <Animated
+                    animationIn="slideInDown"
+                    animationOut="zoomOutDown"
+                    isVisible={true}
+                  >
+                    {/* <h1> BigFoot Avatar </h1> */}
+                    <img
+                      className="neutralBigFoot"
+                      src={neutralBigFoot}
+                      alt="logo"
+                    />
+                  </Animated>
+                </div>
 
                 <div className="col-md-8 text-center">
+                  <Animated
+                    animationIn="slideInRight"
+                    animationOut="zoomOutDown"
+                    isVisible={true}
+                  >
+                    <Card>
+                      <h1 id="dashboardTitle"> Achievements </h1>
 
-                <Animated animationIn="slideInRight" animationOut="zoomOutDown" isVisible={true}>
-                  <Card>
-                    <h1 id= "dashboardTitle"> Achievements </h1>
+                      <Achievement />
 
-                    <Achievement />
-
-
-                    {/* <CardBody>
+                      {/* <CardBody>
       <CardTitle>Card title</CardTitle>
       <CardSubtitle>Card subtitle</CardSubtitle>
       <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
 
     </CardBody> */}
-                  </Card>
+                    </Card>
                   </Animated>
-
                 </div>
               </div>
             </div>
 
-            <hr>
-            </hr>
-            <Animated animationIn="fadeInUp" animationOut="zoomOutDown" isVisible={true}>
+            <hr />
+            <Animated
+              animationIn="fadeInUp"
+              animationOut="zoomOutDown"
+              isVisible={true}
+            >
               <div className="row">
                 <div className="col-12">
                   {/* <div className="container"> */}
                   <div className="ProgressBar">
                     <Card>
-                      <h1 id= "dashboardTitle"> Progress Bar </h1>
+                      <h1 id="dashboardTitle"> Progress Bar </h1>
                       <ProgressBar />
-
                     </Card>
-
                   </div>
                   {/* </div> */}
                 </div>
               </div>
             </Animated>
 
-            <hr>
-            </hr>
-
+            <hr />
 
             <div className="CashFlow">
               <div className="row">
-
                 <div className="col-12 text-center">
-                <Zoom>
-                  <Card>
-<<<<<<< HEAD
-                    <h1 id= "dashboardTitle"> Cash Flow </h1>
-                    <CashFlow />
-=======
+                  <Zoom>
+                    <Card>
+                      <h1 id="dashboardTitle"> Cash Flow </h1>
+                      <CashFlow />
 
-                    <h1> Cash Flow </h1>
-  <CashFlow />
->>>>>>> b281dac60f6710e22e2468803ce911704ab81793
-
-
-                    {/* <CardBody>
+                      {/* <CardBody>
                       <CardTitle>Card title</CardTitle>
                       <CardSubtitle>Card subtitle</CardSubtitle>
                       <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
 
                     </CardBody> */}
-                  </Card>
-               
-               </Zoom>
+                    </Card>
+                  </Zoom>
                 </div>
               </div>
             </div>
@@ -135,78 +119,49 @@ class Dashboard extends Component {
               </Card>
             </div> */}
 
-            <hr>
-
-            </hr>
+            <hr />
 
             <div className="TransactionDetail">
-
               <div className="row">
                 <div className="col-md-8">
+                  <Zoom>
+                    <Card>
+                      <h1 id="dashboardTitle"> Transaction Detail</h1>
 
-                <Zoom>
-                  <Card>
-<<<<<<< HEAD
-                    <h1 id= "dashboardTitle"> Transaction Detail</h1>
-=======
+                      <TransactionDetail />
 
-                    <h1> Transaction Detail</h1>
->>>>>>> b281dac60f6710e22e2468803ce911704ab81793
-
-
-                    <TransactionDetail />
-
-                    {/* <CardBody>
+                      {/* <CardBody>
                       <CardTitle>Card title</CardTitle>
                       <CardSubtitle>Card subtitle</CardSubtitle>
                       <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
 
                     </CardBody> */}
-                  </Card>
-<<<<<<< HEAD
-                </Zoom>
-=======
-
-                </Animated>
-
-
-
-   
-
->>>>>>> b281dac60f6710e22e2468803ce911704ab81793
+                    </Card>
+                  </Zoom>
                 </div>
 
                 <div className="col-md-4">
-                <Fade right>
-                {/* <Animated animationIn="slideInRight" animationOut="zoomOutDown" isVisible={true}> */}
-                  <Card>
-                    <h1 id= "dashboardTitle"> Total Spending</h1>
-                    <TotalSpending />
+                  <Fade right>
+                    {/* <Animated animationIn="slideInRight" animationOut="zoomOutDown" isVisible={true}> */}
+                    <Card>
+                      <h1 id="dashboardTitle"> Total Spending</h1>
+                      <TotalSpending />
 
-
-                    {/* <CardBody>
+                      {/* <CardBody>
                       <CardTitle>Card title</CardTitle>
                       <CardSubtitle>Card subtitle</CardSubtitle>
                       <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
 
                     </CardBody> */}
-                  </Card>
-<<<<<<< HEAD
-                {/* </Animated> */}
-                </Fade>
-=======
-
-                </Animated>
-
->>>>>>> b281dac60f6710e22e2468803ce911704ab81793
+                    </Card>
+                    {/* </Animated> */}
+                  </Fade>
                 </div>
-
               </div>
             </div>
           </div>
         </div>
-      </div >
-
+      </div>
     );
   }
 }
