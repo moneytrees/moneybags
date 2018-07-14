3c25c884ecaed45a8131072eb6d2a703b156dc71
Connor Thomas <connor.ian.thomas@gmail.com>
2018-07-14 14:32:12 -0500

    fixing footer issue

--- client/src/Pages/Unprotected/Landing/Landing.js
+++ client/src/Pages/Unprotected/Landing/Landing.js
@@ -3,7 +3,7 @@ import './Landing.css';
 /* import Nav from "../../../components/nav" */
 import Hero from "../../../components/hero/Hero"
 import AboutFeatures from "../../../components/aboutFeatures/AboutFeatures"
-import Footer from "../../../components/footer"
+import Footer from "../../../components/Footer/Footer"
 
 class Landing extends Component {
   render() {
