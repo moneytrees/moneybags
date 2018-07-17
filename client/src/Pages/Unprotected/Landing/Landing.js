21c1e6c4e93176295ec2e88fd9929d61bebbec4f
Chris Jones <chrisjones0517@gmail.com>
2018-07-16 21:11:40 -0500

    corrected lowercase folder name 'aboutFeatures' to 'AboutFeatures' in ./src/Pages/Unprotected/Landing/Landing.js

--- client/src/Pages/Unprotected/Landing/Landing.js
+++ client/src/Pages/Unprotected/Landing/Landing.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Landing.css';
 import Hero from "../../../components/hero/Hero"
-import AboutFeatures from "../../../components/aboutFeatures/AboutFeatures";
+import AboutFeatures from "../../../components/AboutFeatures/AboutFeatures";
 import Footer from "../../../components/Footer"
 
 class Landing extends Component {
